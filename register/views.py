cf7ab20b23844a8893d05fb798b382ede94543ce
Rashmi Deshmukh <rashdeshmukh93@gmail.com>
2023-11-05 19:30:07 +0530

    send email ti registered user

--- register/views.py
+++ register/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Registeruser
 from  passlib.hash import pbkdf2_sha256
+from django.conf import settings
+from django.core.mail import send_mail
 
 
 # Create your views here.
@@ -25,15 +27,19 @@ def register(request):
             Registeruser.objects.get(email=email)
             
         except:
-            context={'data':"Email already exisis"}
-            return render(request,"register_result.html",context)
-        else:
             user_data=Registeruser(name=name,email=email,phone=phone,password=enc_pass)
             user_data.save()
             context={'data':name}
+
+            subject = 'welcome to RD channel'
+            message = f'Hi {name}, thank you for registering with us'
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = [email]
+            send_mail( subject, message, email_from, recipient_list )
+
             return render(request,"register_result.html",context)
-          
-        
-        
-    
+        else:
+            context={'data':"User already register"}
+            return render(request,"register_result.html",context)
+
     return render(request,"register.html")
