c02e501695fe1db95b172d763037353ebc69acef
Rashmi Deshmukh <rashdeshmukh93@gmail.com>
2023-11-05 19:40:00 +0530

    remove setting

--- signin_singup/settings.py
+++ signin_singup/settings.py
@@ -136,8 +136,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rashdeshmukh93@gmail.com'
-EMAIL_HOST_PASSWORD = 'spijczubabybjcsq'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'app_password'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
